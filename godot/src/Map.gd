f24e2bd724732746f427829fa0166b10ec718a02
Kolja Blauhut <kodeeey@gmail.com>
2020-02-18 21:58:03 +0100

    -Updated lighting

--- godot/src/Map.gd
+++ godot/src/Map.gd
@@ -3,7 +3,7 @@ extends Node
 var tile_scene = load("res://src/tiles/NeonLevel_1.tscn")
 
 const TILE_SIZE = 20
-const LEVEL_LENGHT = 10
+const LEVEL_LENGHT = 7
 const LEVEL_WIDTH = 1
 
 var tileX = -1
