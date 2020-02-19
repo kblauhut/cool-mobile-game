b4e043a37fa83da5934c8309f6118fe2b732c378
Kolja Blauhut <koljablauhut@gmail.com>
2020-02-19 14:19:04 +0100

    -Updated level and lighting

--- godot/src/Map.gd
+++ godot/src/Map.gd
@@ -3,13 +3,11 @@ extends Node
 var tile_scene = load("res://src/tiles/NeonLevel_1.tscn")
 
 const TILE_SIZE = 20
-const LEVEL_LENGHT = 7
+const LEVEL_LENGHT = 4
 const LEVEL_WIDTH = 1
 
 var tileX = -1
 var tileZ = -1
-
-var current_map = []
 
 func _ready():
 	spawn_tiles()
@@ -33,7 +31,6 @@ func spawn_tiles():
 	var z = int(floor(get_parent().get_node("Player").get_translation().z))
 	var y = int(floor(get_parent().get_node("Player").get_translation().y))
 	var new_coordinates = coordinates()
-	print(new_coordinates)
 	
 	if new_coordinates[0] > tileX:
 		tileX = new_coordinates[0]
@@ -42,7 +39,6 @@ func spawn_tiles():
 				var new_tile = tile_scene.instance()
 				var offsetX = x + TILE_SIZE/2 + i * TILE_SIZE
 				var offsetZ = z + TILE_SIZE * (n - floor(LEVEL_WIDTH/2))
-				
 				new_tile.set_translation(Vector3(offsetX, 0, 0))
 				new_tile.set_name("tile")
 				self.get_parent().call_deferred("add_child", new_tile)
